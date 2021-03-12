c0c25d2e7fefc60628177e364b8c7123d9b59b8a
pbed <42191325+pbed@users.noreply.github.com>
2021-03-12 20:07:43 +0100

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,10 +1,10 @@
 export default {
   cake: {
-    56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+    56: '0xd592dfe9e1b687b1d9dbdacf5b9f0789cc9cd584',
     97: '',
   },
   masterChef: {
-    56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
+    56: '0x8881C9E351482dDf0F3186A33fb40E28c0a52A26',
     97: '',
   },
   wbnb: {
@@ -21,7 +21,7 @@ export default {
   },
   mulltiCall: {
     56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
-    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+    97: '',
   },
   busd: {
     56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
