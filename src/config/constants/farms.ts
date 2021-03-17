cc7510f636dae64b104983bd688f36de6f1b63a2
pbed <42191325+pbed@users.noreply.github.com>
2021-03-17 19:19:31 +0100

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -187,7 +187,7 @@ const farms: FarmConfig[] = [
       97: '',
       56: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019', // EGG-BUSD LP
     },
-    tokenSymbol: 'EGG',
+    tokenSymbol: 'EGG O',
     tokenAddresses: {
       97: '',
       56: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
